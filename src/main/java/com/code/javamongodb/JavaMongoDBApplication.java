e504851492f88bbd5748a2d9db0e6de5533bb6cf
Sergio Silva <sergio.silva.arancibia@gmail.com>
2024-01-18 00:00:37 -0300

    Se modifica nombre de archivo

--- src/main/java/com/code/javamongodb/JavaMongoDBApplication.java
+++ src/main/java/com/code/javamongodb/JavaMongoDBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamplejavaApplication {
+public class JavaMongoDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExamplejavaApplication.class, args);
+		SpringApplication.run(JavaMongoDBApplication.class, args);
 	}
 
 }
